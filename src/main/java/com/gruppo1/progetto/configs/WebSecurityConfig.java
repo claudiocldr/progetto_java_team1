ba636074647695c50ad524f7278628fa033601dc
claudiocldr <claudiocaldarella13@gmail.com>
2023-07-27 15:01:50 +0200

    feat: added security connected to db, have to add persistence

--- src/main/java/com/gruppo1/progetto/configs/WebSecurityConfig.java
+++ src/main/java/com/gruppo1/progetto/configs/WebSecurityConfig.java
@@ -1,8 +1,14 @@
 package com.gruppo1.progetto.configs;
 
+import com.gruppo1.progetto.repositories.UserRepository;
+import jakarta.activation.DataSource;
+import jakarta.servlet.annotation.ServletSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,18 +16,33 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.management.relation.Role;
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    private final UserRepository userRepository;
+    @Autowired
+    public WebSecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+        List<UserDetails> listOfUsers = new ArrayList<>();
 
-        return new InMemoryUserDetailsManager(user);
+        for (com.gruppo1.progetto.models.User user : userRepository.findAll()) {
+            UserDetails userLogin =
+                    User.withDefaultPasswordEncoder().username(user.getUsername())
+                            .password(user.getPassword())
+                            .roles("USER")
+                            .build();
+            listOfUsers.add(userLogin);
+        }
+        return new InMemoryUserDetailsManager(listOfUsers);
     }
 }
--- src/main/java/com/gruppo1/progetto/models/User.java
+++ src/main/java/com/gruppo1/progetto/models/User.java
@@ -27,6 +27,9 @@ public class User {
         this.authority = authority;
     }
 
+    public User() {
+    }
+
     public Authority getAuthority() {
         return authority;
     }
