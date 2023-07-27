f642464880d0cf31ff82586a1ce6b2ad14b05cd7
claudiocldr <claudiocaldarella13@gmail.com>
2023-07-27 19:10:28 +0200

    rollback to just controllers

--- src/main/java/com/gruppo1/progetto/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gruppo1.progetto.repositories;
-
-import com.gruppo1.progetto.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-}
