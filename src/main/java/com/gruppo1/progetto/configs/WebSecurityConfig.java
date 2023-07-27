a71b8d5589190e97733e803dfece0b8d6dafbb3b
claudiocldr <claudiocaldarella13@gmail.com>
2023-07-27 15:02:24 +0200

    chore: removed some imports

--- src/main/java/com/gruppo1/progetto/configs/WebSecurityConfig.java
+++ src/main/java/com/gruppo1/progetto/configs/WebSecurityConfig.java
@@ -1,22 +1,15 @@
 package com.gruppo1.progetto.configs;
 
 import com.gruppo1.progetto.repositories.UserRepository;
-import jakarta.activation.DataSource;
-import jakarta.servlet.annotation.ServletSecurity;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import javax.management.relation.Role;
 import java.util.ArrayList;
 import java.util.List;
 
